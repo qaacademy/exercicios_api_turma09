8c6876f33e348a9d89c16aaaf722c431f4fa8b51
Flavio Dias <flavio.goncalvesdias@gmail.com>
2024-06-01 09:48:39 -0300

    Exercicio02 - Primeira Api

--- src/test/java/TestePrimeiraApi.java
+++ src/test/java/TestePrimeiraApi.java
@@ -1,11 +1,12 @@
 import io.restassured.RestAssured;
+import org.apache.http.Header;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 
 public class TestePrimeiraApi {
 
     @Test
-    public void testeComSucessoPrimeiraApi() {
+    public void exercicio01_TesteComSucessoPrimeiraApi() {
 
         String url = "http://localhost:8080/api/primeiraApi";
 
@@ -18,6 +19,25 @@ public class TestePrimeiraApi {
                 .assertThat()
                 .statusCode(200)
                 .body(Matchers.containsString("Retorno da api com sucesso!!!"));
-
     }
+
+    @Test
+    public void exercicio02_TesteQueryParamPrimeiraApi() {
+
+        String url = "http://localhost:8080/api/primeiraApiV1";
+        String textoQueryParam = "QA Academy";
+
+        RestAssured.given()
+                .queryParam("palavra",textoQueryParam)
+                .log().all()
+                .when()
+                .get(url)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body(Matchers.containsString(textoQueryParam));
+    }
+
+
 }
