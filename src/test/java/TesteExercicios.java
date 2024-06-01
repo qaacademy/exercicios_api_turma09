1e0c137d327a9f72c7b1b3612d4f0ae274c3b9db
Flavio Dias <flavio.goncalvesdias@gmail.com>
2024-06-01 10:47:05 -0300

    Exercicio 05 - Endpoint exercicios

--- src/test/java/TesteExercicios.java
+++ src/test/java/TesteExercicios.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class TesteExercicios {
 
     @Test
-    public void exercicio01_TesteParOuImpar() {
+    public void exercicio04_TesteParOuImpar() {
 
         String url = "http://localhost:8080/exercicios/parOuImpar";
         String msgResponseBody = "O numero 2 é par";
@@ -20,5 +20,22 @@ public class TesteExercicios {
                 .assertThat()
                 .statusCode(200)
                 .body(Matchers.equalTo(msgResponseBody));
+    }
+
+    @Test
+    public void exercicio05_TesteParOuImpar() {
+
+        String pathParamSalario = "3000";
+        String url = "http://localhost:8080/exercicios/calculaSalario/"+pathParamSalario;
+
+        RestAssured.given()
+                .log().all()
+                .when()
+                .get(url)
+                .then()
+                .log().all()
+                .assertThat()
+                .statusCode(200)
+                .body(Matchers.containsString("Salario liquido = 2050.65"));
     }
 }
